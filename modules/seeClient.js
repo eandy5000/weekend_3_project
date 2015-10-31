589065e9858972aeb3950272ca9ed9ecdce198c9
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2015-10-31 11:17:09 -0500

    trying to connect client and server

---
+++ modules/seeClient.js
@@ -0,0 +1,3 @@
+/**
+ * Created by eandreweccleston on 10/31/15.
+ */
--- server/public/assets/scripts/scripts.js
+++ server/public/assets/scripts/scripts.js
@@ -3,7 +3,6 @@
  */
 $(document).ready(function(){
 
-    console.log("doc ready");
 
 
 init();
@@ -21,8 +20,28 @@ function init(){
 
 function addSubmitButton(){
     $('#inputForm').append("<div class='submittButton'>Submitt</div>");
-    $('#inputForm').on('click', 'submittButton',submittData);
+    $('#inputForm').on('click', '.submittButton',submittData);
 
 }
 
 // client-server functions
+
+function submittData(){
+
+    var formObject = {};
+    $.each($('#inputForm').serializeArray(), function(i, field){
+        formObject[field.name] = field.value;
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "/upload",
+        data: formObject,
+        beforeSend: function() {
+            console.log("the upload", formObject);
+        },
+        success: function(data){
+            console.log("data pointing to formObject in success ", data);
+        }
+    })
+}
